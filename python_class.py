3aa0c6402f7ce80aedb63e117565dbd741edfdc2
kashif <kashifiqbal0214@gmail.com>
2023-06-08 17:20:58 +0500

    inheritance class is made from parent class

--- python_class.py
+++ python_class.py
@@ -1,13 +1,15 @@
 class Parent:
-    def __init__(self,name, age, grade):
+    def __init__(self,name, age, hair_color):
         self.name = name
         self.age = age
-        self.grade = grade 
+        self.hair_color = hair_color
 
-    
+#inheritance    
 class Child(Parent):
-    super.__init__
+    def __init__(self, name, age, hair_color):
+        super().__init__(name, age, hair_color)
 
 
-p1 = Parent("Ali",30,16)
-
+p1 = Parent("Ali",30,"black")
+ch = Child('Asif',5,"black")
+print("Father name is",p1.name, "and his child name is",ch.name, "father age is",p1.age, "and his son age is ", ch.age, ".")
