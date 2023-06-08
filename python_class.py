711fb7e6bb428a925014910e3c8260a3231b9466
kashif <kashifiqbal0214@gmail.com>
2023-06-08 17:03:03 +0500

    pytho_class.py file is created

---
+++ python_class.py
@@ -0,0 +1,13 @@
+class Parent:
+    def __init__(self,name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade 
+
+    
+class Child(Parent):
+    super.__init__
+
+
+p1 = Parent("Ali",30,16)
+
